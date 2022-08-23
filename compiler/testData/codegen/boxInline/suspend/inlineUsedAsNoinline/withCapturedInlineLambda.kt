8738dd050624b918554954ab09e514d7076c8b95
Abduqodiri Qurbonzoda <abduqodiri.qurbonzoda@jetbrains.com>
2022-08-23 11:23:25 +0300

    Specialize Flow and FlowCollector type parameter with String

--- compiler/testData/codegen/boxInline/suspend/inlineUsedAsNoinline/withCapturedInlineLambda.kt
+++ compiler/testData/codegen/boxInline/suspend/inlineUsedAsNoinline/withCapturedInlineLambda.kt
@@ -6,28 +6,28 @@
 // FILE: inlined.kt
 package flow
 
-interface Flow<T> {
-    suspend fun collect(collector: FlowCollector<T>)
+interface Flow {
+    suspend fun collect(collector: FlowCollector)
 }
 
-interface FlowCollector<T> {
-    suspend fun emit(value: T)
+interface FlowCollector {
+    suspend fun emit(value: String)
 }
 
-inline fun <T> flow(crossinline body: suspend FlowCollector<T>.() -> Unit): Flow<T> =
-    object : Flow<T> {
-        override suspend fun collect(collector: FlowCollector<T>) = collector.body()
+inline fun flow(crossinline body: suspend FlowCollector.() -> Unit): Flow =
+    object : Flow {
+        override suspend fun collect(collector: FlowCollector) = collector.body()
     }
 
-suspend inline fun <T> Flow<T>.collect(crossinline body: suspend (T) -> Unit) =
-    collect(object : FlowCollector<T> {
-        override suspend fun emit(value: T) = body(value)
+suspend inline fun Flow.collect(crossinline body: suspend (String) -> Unit) =
+    collect(object : FlowCollector {
+        override suspend fun emit(value: String) = body(value)
     })
 
-inline fun <T, R> Flow<T>.transform(crossinline transformer: suspend FlowCollector<R>.(value: T) -> Unit): Flow<R> =
-    flow<R> { collect { value -> transformer(value) } }
+inline fun Flow.transform(crossinline transformer: suspend FlowCollector.(value: String) -> Unit): Flow =
+    flow { collect { value -> transformer(value) } }
 
-inline fun <T, R> Flow<T>.map(crossinline transformer: suspend (value: T) -> R): Flow<R> =
+inline fun Flow.map(crossinline transformer: suspend (value: String) -> String): Flow =
     transform { value -> emit(transformer(value)) }
 
 // FILE: box.kt
@@ -47,9 +47,9 @@ var result = ""
 
 fun box(): String {
     suspend {
-        val source = flow<String> { emit("O") }
-        val reference: (suspend (String) -> String) -> Flow<String> = source::map
-        ((reference as KFunction<*>).javaMethod!!.invoke(null, source, ::addK) as Flow<String>).collect { result = it }
+        val source = flow { emit("O") }
+        val reference: (suspend (String) -> String) -> Flow = source::map
+        ((reference as KFunction<*>).javaMethod!!.invoke(null, source, ::addK) as Flow).collect { result = it }
     }.startCoroutine(EmptyContinuation)
     return result
 }
