8cef4696f4e3c096c4b158518d21e8784dbb35e6
Yahor Berdnikau <yahor.berdnikau@jetbrains.com>
2022-08-26 10:01:08 +0200

    Fix Gradle configuration were created on execution phase

--- buildSrc/src/main/kotlin/tasks.kt
+++ buildSrc/src/main/kotlin/tasks.kt
@@ -11,7 +11,6 @@ import com.sun.management.OperatingSystemMXBean
 import org.gradle.api.Project
 import org.gradle.api.Task
 import org.gradle.api.artifacts.Configuration
-import org.gradle.api.artifacts.ProjectDependency
 import org.gradle.api.file.FileSystemOperations
 import org.gradle.api.internal.tasks.testing.filter.DefaultTestFilter
 import org.gradle.api.tasks.TaskProvider
@@ -282,28 +281,23 @@ object TaskUtils {
 }
 
 private fun Task.useAndroidConfiguration(systemPropertyName: String, configName: String) {
-    val configuration = with(project) {
-        configurations.getOrCreate(configName)
-            .also {
-                if (it.allDependencies.matching { dep ->
-                        dep is ProjectDependency &&
-                                dep.targetConfiguration == configName &&
-                                dep.dependencyProject.path == ":dependencies:android-sdk"
-                    }.count() == 0) {
-                    dependencies.add(
-                        configName,
-                        dependencies.project(":dependencies:android-sdk", configuration = configName)
-                    )
-                }
+    val configuration = project.configurations.findByName(configName)
+        ?: project.configurations.create(configName) {
+            defaultDependencies {
+                addLater(
+                    project.provider {
+                        project.dependencies.project(":dependencies:android-sdk", configuration = configName)
+                    }
+                )
             }
-    }
+        }
 
     dependsOn(configuration)
 
     if (this is Test) {
-        val androidFilePath = configuration.singleFile.canonicalPath
+        val androidFilePath = project.provider { configuration.singleFile.canonicalPath }
         doFirst {
-            systemProperty(systemPropertyName, androidFilePath)
+            systemProperty(systemPropertyName, androidFilePath.get())
         }
     }
 }
