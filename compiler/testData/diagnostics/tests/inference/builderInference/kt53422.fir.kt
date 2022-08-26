930bdaef83df8a148d7cbafdf2b63ded45cf0ddd
Simon Ogorodnik <simon.ogorodnik@jetbrains.com>
2022-08-26 04:03:17 +0200

    Fix test data for multi lambda

--- compiler/testData/diagnostics/tests/inference/builderInference/kt53422.fir.kt
+++ compiler/testData/diagnostics/tests/inference/builderInference/kt53422.fir.kt
@@ -2,7 +2,7 @@
 fun test() {
     foo(
         flow { emit(0) }
-    ) { it.collect {} }
+    ) { it.collect <!TOO_MANY_ARGUMENTS!>{}<!> }
 
     // 0. Initial
     // W <: Any / declared upper bound
